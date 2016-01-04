5c4bcf189708c052a9c21f434c45e2a7201f0ee6
Iiro Jäppinen <iiro@jappinen.fi>
2016-01-04 10:10:46 +0200

    create some functions and clean up

--- public/app-debug.js
+++ public/app-debug.js
@@ -1,60 +1,44 @@
-// Define available pages
-var pages = [{
-  name: 'About',
-  template: 'about--template',
-  title: 'Iiro Jäppinen',
-  url: '/'
-},{
-  name: 'Résumé',
-  template: 'resume--template',
-  title: 'Résumé of Iiro Jäppinen',
-  url: '/resume'
-}]
+var pages     = [{
+                  name: 'About',
+                  template: 'about--template',
+                  title: 'Iiro Jäppinen',
+                  url: '/'
+                },{
+                  name: 'Résumé',
+                  template: 'resume--template',
+                  title: 'Résumé of Iiro Jäppinen',
+                  url: '/resume'
+                }],
+    page      = pages.filter(function(x) {return x.url == window.location.pathname})[0]
+    container = document.getElementById('container'),
+    nav       = document.getElementById('navigation'),
+    navUl     = nav.getElementsByTagName('ul')[0],
+    navArray  = []
 
-// Create tabs
-var tabContainer = document.getElementById('navigation').getElementsByTagName('ul')[0],
-    tabs         = []
-tabContainer.innerText = ''
-for (var i in pages) {
-  var li = document.createElement('li'),
-      a  = document.createElement('a')
-      t  = document.createTextNode(pages[i].name)
-  li.appendChild(a).appendChild(t)
-  a.setAttribute('href', pages[i].url)
-  a.setAttribute('navigation','')
-  tabContainer.appendChild(li)
-  tabs.push(a)
-}
-
-// Set initial page and history.state
-var page = pages.filter(function(x) {return x.url == window.location.pathname})[0]
+navUl.innerText = ''
 history.replaceState(page, page.title, page.url)
 
-// Listen to scroll and add shadows to navigation bar
-if (window.scrollY > 0) {
-  document.getElementById('navigation').setAttribute('opaque', '')
+function createNav(i) {
+  var navLi = document.createElement('li'),
+      navA  = document.createElement('a')
+      navAT = document.createTextNode(pages[i].name)
+  navLi.appendChild(navA).appendChild(navAT)
+  navA.setAttribute('href', pages[i].url)
+  navA.setAttribute('navigation','')
+  navUl.appendChild(navLi)
+  navArray.push(navA)
 }
-window.addEventListener('scroll', function() {
-  var nav = document.getElementById('navigation')
-  if (this.scrollY > 0) {
-    nav.setAttribute('opaque', '')
-  } else {
-    nav.removeAttribute('opaque')
-  }
-}, false)
 
-// Update active tab
-function updateTabs() {
-  for (var i in tabs) {
-    if (tabs[i].getAttribute('href') == page.url) {
-      tabs[i].setAttribute('active','')
+function activeNavA() {
+  for (var i in navArray) {
+    if (navArray[i].getAttribute('href') == page.url) {
+      navArray[i].setAttribute('active','')
     } else {
-      tabs[i].removeAttribute('active','')
+      navArray[i].removeAttribute('active','')
     }
   }
 }
 
-// Re-render page on navigation links
 function navigation(x) {
   if (x !== page.url) {
     page = pages.filter(function(y) {return y.url == x})[0]
@@ -63,31 +47,40 @@ function navigation(x) {
   }
 }
 
-// Render page
 function render(x) {
-  var template  = document.getElementById(page.template),
-      clone     = document.importNode(template.content, true),
-      container = document.getElementById('container')
-
+  var template = document.getElementById(page.template),
+      clone    = document.importNode(template.content, true)
   while (container.childElementCount !== 0) { container.children[0].remove() }
   container.appendChild(clone)
   document.title = page.title
-  updateTabs()
+  activeNavA()
 
-  nav = Array.prototype.slice.call(document.querySelectorAll('a[navigation]'))
-  for (var i in nav) {
-    nav[i].addEventListener('click', function(event) {
+  var links = Array.prototype.slice.call(document.querySelectorAll('a[navigation]'))
+  for (var i in links) {
+    links[i].addEventListener('click', function(event) {
       event.preventDefault()
       navigation(this.getAttribute('href'))
     })
   }
 }
 
-// Initial page load
+for (var i in pages) {
+  createNav(i)
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  if (window.scrollY > 0) {
+    nav.setAttribute('opaque', '')
+  }
   render(page.url)
 })
-// Window history page load
+window.addEventListener('scroll', function() {
+  if (this.scrollY > 0) {
+    nav.setAttribute('opaque', '')
+  } else {
+    nav.removeAttribute('opaque')
+  }
+}, false)
 window.onpopstate = function(event) {
   if ( page !== history.state) {
     page = history.state
