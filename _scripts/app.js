3345aa8b39d871f3bef3e151a31164d876b1dad5
Iiro Jäppinen <iiro@jappinen.fi>
2016-01-24 07:31:09 +0200

    use map functions

--- _scripts/app.js
+++ _scripts/app.js
@@ -1,44 +1,18 @@
 var pages     = [{
-                  name: 'About',
-                  template: 'about--template',
-                  title: 'Iiro Jäppinen',
-                  url: '/'
+                  name: "About",
+                  template: "about--template",
+                  title: "Iiro Jäppinen",
+                  url: "/"
                 },{
-                  name: 'Résumé',
-                  template: 'resume--template',
-                  title: 'Résumé of Iiro Jäppinen',
-                  url: '/resume'
+                  name: "Résumé",
+                  template: "resume--template",
+                  title: "Résumé of Iiro Jäppinen",
+                  url: "/resume"
                 }],
     page      = pages.filter(function(x) {return x.url == window.location.pathname})[0],
-    container = document.getElementById('container'),
-    nav       = document.getElementById('navigation'),
-    navUl     = nav.getElementsByTagName('ul')[0],
+    nav       = document.getElementById("navigation"),
+    navUl     = nav.getElementsByTagName("ul")[0],
     navArray  = []
-
-function createNav(i) {
-  var navLi = document.createElement('li'),
-      navA  = document.createElement('a')
-      navAT = document.createTextNode(pages[i].name)
-  navLi.appendChild(navA).appendChild(navAT)
-  navA.setAttribute('href', pages[i].url)
-  navA.setAttribute('navigation','')
-  navA.addEventListener('click', function(event) {
-    event.preventDefault()
-    navigation(this.getAttribute('href'))
-  })
-  navUl.appendChild(navLi)
-  navArray.push(navA)
-}
-
-function activeNavA() {
-  for (var i in navArray) {
-    if (navArray[i].getAttribute('href') == page.url) {
-      navArray[i].setAttribute('active','')
-    } else {
-      navArray[i].removeAttribute('active','')
-    }
-  }
-}
 
 function navigation(x) {
   if (x !== page.url) {
@@ -49,36 +23,52 @@ function navigation(x) {
 }
 
 function render(x) {
-  var template = document.getElementById(page.template),
-      clone    = document.importNode(template.content, true),
-      child
+  var container = document.getElementById("container"),
+      template  = document.getElementById(page.template),
+      clone     = document.importNode(template.content, true)
   while (container.childElementCount !== 0) {
-    child = container.children[0]
-    child.remove()
+    container.children[0].remove()
   }
   container.appendChild(clone)
-  activeNavA()
+  navArray.map(function(x) {
+    if (x.getAttribute("href") == page.url) {
+      x.setAttribute("active", "")
+    } else {
+      x.removeAttribute("active")
+    }
+  })
 }
 
-navUl.innerHTML = ''
 history.replaceState(page, page.title, page.url)
 
-for (var i in pages) {
-  createNav(i)
-}
+navUl.innerHTML = ""
 
-document.addEventListener('DOMContentLoaded', function() {
-  if (window.scrollY > 0) {
-    nav.setAttribute('opaque', '')
-  }
-  render(page.url)
+pages.map(function(x) {
+  var navLi = document.createElement("li"),
+      navA  = document.createElement("a"),
+      navAT = document.createTextNode(x.name)
+  navLi.appendChild(navA).appendChild(navAT)
+  navA.setAttribute("href", x.url)
+  navA.setAttribute("navigation","")
+  navA.addEventListener("click", function(event) {
+    event.preventDefault()
+    navigation(this.getAttribute("href"))
+  })
+  navUl.appendChild(navLi)
+  navArray.push(navA)
 })
 
-window.addEventListener('scroll', function() {
+if (window.scrollY > 0) {
+  nav.setAttribute("opaque", "")
+}
+
+render(page.url)
+
+window.addEventListener("scroll", function() {
   if (this.scrollY > 0) {
-    nav.setAttribute('opaque', '')
+    nav.setAttribute("opaque", "")
   } else {
-    nav.removeAttribute('opaque')
+    nav.removeAttribute("opaque")
   }
 }, false)
 
