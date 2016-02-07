7f6fb941f412d12437bc3d4d64ab395c052a28c1
Iiro Jäppinen <iiro@jappinen.fi>
2016-02-07 16:18:53 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,69 @@
 // Load plugins
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
     cssnano      = require("gulp-cssnano"),
-    inline       = require('gulp-inline-source'),
-    minify       = require('gulp-minify'),
-    base64       = require('gulp-base64'),
-    rename       = require('gulp-rename'),
-    concat       = require('gulp-concat'),
-    webserver    = require('gulp-webserver')
+    inline       = require("gulp-inline-source"),
+    minify       = require("gulp-minify"),
+    base64       = require("gulp-base64"),
+    rename       = require("gulp-rename"),
+    concat       = require("gulp-concat"),
+    webserver    = require("gulp-webserver")
 
-gulp.task('styles', function() {
-  return gulp.src('_styles/default.scss')
+gulp.task("styles", function() {
+  return gulp.src("_styles/default.scss")
     .pipe(sass())
-    .pipe(concat('main.css'))
-    .pipe(autoprefixer('last 2 version'))
+    .pipe(concat("main.css"))
+    .pipe(autoprefixer("last 2 version"))
     .pipe(cssnano())
     .pipe(base64({
             baseDir: "_media",
-            extensions: ['jpg', 'svg']
+            extensions: ["svg"]
      }))
-    .pipe(gulp.dest('_static'))
+    .pipe(gulp.dest("_static"))
 })
 
-gulp.task('scripts', function() {
-  gulp.src('_scripts/*.js')
-    .pipe(concat('app.js'))
+gulp.task("scripts", function() {
+  gulp.src("_scripts/*.js")
+    .pipe(concat("app.js"))
     .pipe(minify({
       ext:{
-            src:'-debug.js',
-            min:'.js'
+            src:"-debug.js",
+            min:".js"
         },
     }))
-    .pipe(gulp.dest('_static'))
+    .pipe(gulp.dest("_static"))
 })
 
-gulp.task('static', ['styles', 'scripts'], function() {
-  gulp.src(['_static/*.png', '_static/*.ico'])
-    .pipe(gulp.dest('public'))
-  gulp.src('_static/*.html')
+gulp.task("static", ["styles", "scripts"], function() {
+  gulp.src(["_static/*.png", "_static/*.ico"])
+    .pipe(gulp.dest("public"))
+  gulp.src("_static/*.html")
     .pipe(inline())
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest("public"))
 })
 
-gulp.task('watch', function() {
-  gulp.watch('_static/*', ['static'])
-  gulp.watch('_styles/*.scss', ['styles'])
-  gulp.watch('_scripts/*.js', ['scripts'])
+gulp.task("watch", function() {
+  gulp.watch("_static/*", ["static"])
+  gulp.watch("_styles/*.scss", ["styles"])
+  gulp.watch("_scripts/*.js", ["scripts"])
 })
 
 gulp.task("webserver", function() {
-  gulp.src('public')
+  gulp.src("public")
     .pipe(webserver({
-      livereload: true,
       directoryListing: false,
+      fallback: "index.html",
+      host: "localhost",
+      livereload: true,
       open: true,
-      proxies: [{source: '/resume', target: 'http://localhost:8000/'}]
+      port: 8000
     }))
 })
 
-gulp.task('default', function() {
-  gulp.start('static')
-  gulp.start('watch')
-  gulp.start('webserver')
+gulp.task("default", function() {
+  gulp.start("static")
+  gulp.start("watch")
+  gulp.start("webserver")
 
 })
