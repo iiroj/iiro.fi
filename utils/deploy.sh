66b16092842cedca1381e14f2eb391b88fe30685
Iiro Jäppinen <iiro@jappinen.fi>
2016-12-27 14:04:04 +0200

    remove --cf-invalidate flag from deploy script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -7,9 +7,8 @@ S3_URL="s3://iiro.fi"
 
 # Deploy to S3
 $S3CMD_EXE sync $BUILD_DIR $S3_URL \
-  --delete-removed         \
-  --guess-mime-type        \
-  --no-mime-magic          \
-  --no-preserve            \
-  --server-side-encryption \
-  --cf-invalidate
+  --delete-removed \
+  --guess-mime-type \
+  --no-mime-magic \
+  --no-preserve \
+  --server-side-encryption
