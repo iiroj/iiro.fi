cc01e4cd69fc1b624ab6f2fedfbddf65a38a4f32
Iiro Jäppinen <iiro@jappinen.fi>
2017-02-16 21:30:39 +0200

    Use awscli instead of s3cmd

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 # Variables
-S3CMD_EXE=$(which s3cmd)
 BUILD_DIR="./public/"
 S3_URL="s3://iiro.fi"
 
 # Deploy to S3
-$S3CMD_EXE sync $BUILD_DIR $S3_URL \
-  --delete-removed \
-  --guess-mime-type \
-  --no-mime-magic \
-  --no-preserve \
-  --server-side-encryption
+aws s3 sync $BUILD_DIR $S3_URL \
+  --delete \
+  --sse AES256
