eced5c4a289acbe5d22ab5e9e19d009e44794bda
Iiro Jäppinen <iiro@jappinen.fi>
2017-03-26 20:57:11 +0300

    Update Icons

--- components/Icons.js
+++ components/Icons.js
@@ -1,43 +1,28 @@
 import React from 'react'
 
+import s from 'icons.module.css'
+
 export const IconEmail = () => (
     <svg
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
+        className={s.icon}
         xmlns="http://www.w3.org/2000/svg" >
         <path
-            d="M27 8l-11 9L1 5h30v22H1V10"
-            strokeWidth="2"
-            stroke="#4D4D4D"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round" />
+            className={s.white}
+            d="M1 5h30v22H1V9" />
+        <path
+            className={s.color}
+            d="M5 9l11 9 10.996-9" />
     </svg>
 )
 
 export const IconLinkedin = () => (
-    <svg
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
-        xmlns="http://www.w3.org/2000/svg" >
-        <g
-            fill="none"
-            fillRule="evenodd" >
-            <path
-                d="M15 26V13v5c0-1 2-5 5-5 2 0 4 2 4 5v8"
-                stroke="#4D4D4D"
-                strokeWidth="2" />
-            <path
-                d="M9 13v18h20c1 0 2-1 2-2.052V3c0-1-1-2-2-2H3C2 1 1 2 1 3v26c0 .687 1 2 2 2h2"
-                stroke="#4D4D4D"
-                strokeWidth="2" />
-            <circle
-                fill="#4D4D4D"
-                cx="9"
-                cy="9"
-                r="1" />
-        </g>
-    </svg>
+    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1 1)" fill="none" fillRule="evenodd"><path d="M3 30h25c1 0 2-1 2-2.052V2c0-1-1-2-2-2H2C1 0 0 1 0 2v25" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M14 24V13v4c0-1 2-5 5-5 2 0 4 2 4 5v7" stroke="#7ACBCB" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"/><circle fill="#7ACBCB" cx="8" cy="8" r="1"/><path d="M8 13v11" stroke="#7ACBCB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></g></svg>
 )
+
+export const IconDribbble = () => (
+    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="2" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"><path d="M15.25 9.008c-2.125-4.062-3.813-5.813-4.813-6.938C7.5 3.196 1 7.508 1 16.008s7.188 15 15 15c4.438 0 6.625-1.688 6.625-1.688s-.313-3.687-2.063-8.5m5.532 6.281c1.31-1.344 3.03-3.125 3.81-5.532" stroke="#FFF"/><path d="M5.063 14.352c2.406 0 7.467-.437 12.937-2.53 5.47-2.096 7.594-4.845 8.844-6.158-2.907-3.186-7.75-5.062-12.374-4.593M8.156 23.883c2-2.22 6-6.313 11.063-7.156 5.06-.846 9.592-.033 11.685.75.282-3.158-.28-5.627-1.53-8.283" stroke="#7ACBCB"/></g></svg>
+)
+
+export const IconGitHub = () => {
+    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 14S1 15 1 17c0 9 6 12 15 12s15-3 15-12c0-4-3-6.5-3-6.5S29 6 28 3c-3 0-7 4-7 4s-2.5-1-5-1-5 1-5 1-4-4-7-4c-1 3 0 7.5 0 7.5" stroke="#FFF" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"/></svg>
+}
