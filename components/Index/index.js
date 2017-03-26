d04181ccbd276047781c8037ef960dfb8675bc41
Iiro Jäppinen <iiro@jappinen.fi>
2017-03-26 09:08:44 +0300

    Add Index component

---
+++ components/Index/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { config } from 'config'
+import styles from './index.module.css'
+
+import Profile from 'Profile'
+
+export default function Index () {
+    return (
+        <article />
+    )
+}
