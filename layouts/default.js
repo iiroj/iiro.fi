f8f3a6928a4d585ea19f3390be44054029fa66b9
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-03 20:34:55 +0300

    Use CSS modules for Template

--- layouts/default.js
+++ layouts/default.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import 'reset.css/reset.css'
+import s from 'styles/_template.module.css'
 
 export default function Template (props) {
     const { children } = props
 
     return (
-      <div id="root">
+      <div className={s.root}>
         {children}
       </div>
     )
