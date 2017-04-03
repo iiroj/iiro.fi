5d7cd720a1b5ac63eb81f8831580f535bd51e513
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-03 19:57:31 +0300

    Updates

--- components/IndexBlogPreview.js
+++ components/IndexBlogPreview.js
@@ -1,27 +1,24 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
-export default (props) => {
-    const latestPost = props.pages.filter( page => {
-        if ( page.file.ext === 'md' ) {
-            return page
-        }
-    }).pop()
-    const latestPostRawDate = latestPost.data.date
-    const latestPostDate = new Date(latestPostRawDate).toDateString()
+export default function IndexBlogPreview (props) {
+    console.log(props)
+    // const latestPostRawDate = latestPost.data.date
+    // const latestPostDate = new Date(latestPostRawDate).toDateString()
 
     return (
-        <section>
-            <h3>From the blog:</h3>
-            <Link to={latestPost.path}>
-                <article>
-                    <h1>{latestPost.data.title}</h1>
-                    <time dateTime={latestPostRawDate}>{latestPostDate}</time>
-                </article>
-            </Link>
-            <nav>
-                <Link to="/blog/">Visit Blog</Link>
-            </nav>
-        </section>
+        null
+        // <section>
+        //     <h3>From the blog:</h3>
+        //     <Link to={latestPost.path}>
+        //         <article>
+        //             <h1>{latestPost.data.title}</h1>
+        //             <time dateTime={latestPostRawDate}>{latestPostDate}</time>
+        //         </article>
+        //     </Link>
+        //     <nav>
+        //         <Link to="/blog/">Visit Blog</Link>
+        //     </nav>
+        // </section>
     )
 }
