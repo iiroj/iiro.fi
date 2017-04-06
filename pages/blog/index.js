2c587c0785547f37deedab88c9bc6b6ccd4f05b4
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-06 20:26:24 +0300

    Make BlogIndex extend React.Component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,35 +2,39 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import Helmet from 'react-helmet'
 
-export default class BlogIndex extends Component {
-    static propTypes = {
-        route: PropTypes.object
-    }
+import s from 'styles/blog.module.css'
 
+export default class BlogIndex extends Component {
     render () {
         const pageLinks = []
-        const siteTitle = this.props.data.site.siteMetadata.siteTitle
+        const name = this.props.data.site.siteMetadata.name
         const posts = this.props.data.allMarkdownRemark.edges
 
         posts.forEach((post) => {
-            if (post.node.path !== '/404/') {
-                const title = post.node.frontmatter.title || post.node.path
-                pageLinks.push(
-                    <li key={post.node.path} >
-                        <Link to={post.node.slug} >
-                            {post.node.frontmatter.title}
-                        </Link>
-                    </li>
-                )
-            }
+            const title = post.node.frontmatter.title
+            pageLinks.push(
+                <li key={post.node.slug} >
+                    <Link to={post.node.slug} >
+                        {post.node.frontmatter.title}
+                    </Link>
+                </li>
+            )
         })
 
         return (
             <div>
-                <Helmet title={this.props.data.site.siteMetadata.title} />
-                <ul>
-                    {pageLinks}
-                </ul>
+                <Helmet title={`Blog of ${name}`} />
+                <nav className={s.back}>
+                    <Link to="/">Back to iiro.fi</Link>
+                </nav>
+                <main>
+                    <header className={s.header}>
+                        <h1 className={s.h1}>Blog</h1>
+                    </header>
+                    <ul className="blog-list">
+                        {pageLinks}
+                    </ul>
+                </main>
             </div>
         )
     }
