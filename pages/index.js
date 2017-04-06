e5fc9eab95680bdd1ad39e447db7893899bd6285
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-06 20:26:54 +0300

    Make Index extend React.Component

--- pages/index.js
+++ pages/index.js
@@ -10,60 +10,63 @@ import IndexBlogPreview from 'components/IndexBlogPreview'
 
 import s from 'styles/index.module.css'
 
-export default function Index (props) {
-    const microdata = `
+export default class Index extends Component {
+    render () {
+        const microdata = `
 {
     '@context': 'http://schema.org',
     '@type': 'Person',
-    'name': '${props.data.site.siteMetadata.name}',
-    'jobTitle': '${props.data.site.siteMetadata.jobTitle}',
-    'worksFor': '${props.data.site.siteMetadata.worksFor}',
-    'url': '${props.data.site.siteMetadata.siteUrl}',
-    'email': '${props.data.site.siteMetadata.email}',
-    'nationality': '${props.data.site.siteMetadata.country}',
+    'name': '${this.props.data.site.siteMetadata.name}',
+    'jobTitle': '${this.props.data.site.siteMetadata.jobTitle}',
+    'worksFor': '${this.props.data.site.siteMetadata.worksFor}',
+    'url': '${this.props.data.site.siteMetadata.siteUrl}',
+    'email': '${this.props.data.site.siteMetadata.email}',
+    'nationality': '${this.props.data.site.siteMetadata.country}',
     'address': {
         '@type': 'PostalAddress',
-        'addressCountry': '${props.data.site.siteMetadata.country}',
-        'addressLocality': '${props.data.site.siteMetadata.city}'
+        'addressCountry': '${this.props.data.site.siteMetadata.country}',
+        'addressLocality': '${this.props.data.site.siteMetadata.city}'
     },
     'sameAs': [
-        '${props.data.site.siteMetadata.linkedin}',
-        '${props.data.site.siteMetadata.dribbble}',
-        '${props.data.site.siteMetadata.github}'
+        '${this.props.data.site.siteMetadata.linkedin}',
+        '${this.props.data.site.siteMetadata.dribbble}',
+        '${this.props.data.site.siteMetadata.github}'
     ]
 }
 `
 
-    return (
-        <main>
-            <article>
-                <Helmet
-                    title={props.data.site.siteMetadata.siteTitle}
-                    script={[{type: 'application/ld+json', innerHTML: microdata }]} />
-                <header className={s.header}>
-                    <div className={s.headerContainer}>
-                        <img
-                            className={s.picture}
-                            src={profile1x}
-                            srcSet={`${profile1x} 1x,
-                            ${profile2x} 2x,
-                            ${profile3x} 3x`} />
-                        <h1 className={s.name}>{props.data.site.siteMetadata.name}</h1>
-                        <h2 className={s.title}>
-                            UX <span className={s.amp}>&</span> UI Designer at <a className={s.fraktio} href="https://fraktio.fi" target="_blank">
-                            <FraktioLogo className={s.fraktioLogo} />
-                            <span>fraktio</span>
-                        </a>
-                    </h2>
-                </div>
-            </header>
-            <div className={s.container}>
-                <Links />
-                <IndexBlogPreview post={props.data.allMarkdownRemark.edges[0].node} />
-            </div>
-        </article>
-    </main>
-)
+        return (
+            <main>
+                <article>
+                    <Helmet
+                        title={this.props.data.site.siteMetadata.name}
+                        script={[{type: 'application/ld+json', innerHTML: microdata }]}
+                    />
+                    <header className={s.header}>
+                        <div className={s.headerContainer}>
+                            <img
+                                className={s.picture}
+                                src={profile1x}
+                                srcSet={`${profile1x} 1x,
+                                ${profile2x} 2x,
+                                ${profile3x} 3x`} />
+                            <h1 className={s.name}>{this.props.data.site.siteMetadata.name}</h1>
+                            <h2 className={s.title}>
+                                UX <span className={s.amp}>&</span> UI Designer at <a className={s.fraktio} href="https://fraktio.fi" target="_blank">
+                                <FraktioLogo className={s.fraktioLogo} />
+                                <span>fraktio</span>
+                                </a>
+                            </h2>
+                        </div>
+                    </header>
+                    <div className={s.container}>
+                        <Links />
+                        <IndexBlogPreview post={this.props.data.allMarkdownRemark.edges[0].node} />
+                    </div>
+                </article>
+            </main>
+        )
+    }
 }
 
 export const pageQuery = `
