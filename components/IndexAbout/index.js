0ec0406e822e645f577be80c23d621daed8acebe
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-09 19:30:29 +0300

    Update About text

--- components/IndexAbout/index.js
+++ components/IndexAbout/index.js
@@ -6,8 +6,9 @@ export const IndexAbout = () => (
     <section>
         <div className={s.container}>
             <p><strong>I’m Iiro Jäppinen</strong>. I design user interfaces, icons and logos. Lately I’ve been defining user stories and turning them into valuable experiences.</p>
-            <p className={s.second}><strong>Previously</strong> I worked at <a href="https://m.verkkokauppa.com" target="_blank" rel="noopener noreferrer"> Verkkokauppa.com</a>, Finland’s largest and best-known online retailer. My goal is to create the best omni-channel shopping experience for all kinds of people.</p>
-            <p className={s.third}><strong>Before that</strong> I helped design the <a href="https://www.humblebundle.com" target="_blank" rel="noopener noreferrer">Humble Indie Bundle</a> brand and concept. I moved to San Francisco in the process but have then returned to Helsinki, Finland. You can reach me online for the occasional icon.</p>
+            <p><strong>I work at</strong> <a href="https://fraktio.fi" target="_blank">Fraktio</a>.</p>
+            <p className={s.second}><strong>Before that</strong> I worked at <a href="https://m.verkkokauppa.com" target="_blank" rel="noopener noreferrer"> Verkkokauppa.com</a>, Finland’s largest and best-known online retailer. I helped create the best omni-channel shopping experience for everyone.</p>
+            <p className={s.third}><strong>Earlier</strong> I helped design the <a href="https://www.humblebundle.com" target="_blank" rel="noopener noreferrer">Humble Indie Bundle</a> brand and concept. I moved to San Francisco in the process but have then returned to Helsinki, Finland. You can reach me online for the occasional icon.</p>
         </div>
     </section>
 )
