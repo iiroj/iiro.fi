25d8466b98a20eb499e5ce309355e7b14e6081fe
Iiro Jäppinen <iiro@jappinen.fi>
2017-04-09 08:50:07 +0300

    Add about text to Index

--- pages/index.js
+++ pages/index.js
@@ -63,6 +63,12 @@ export default class Index extends Component {
                         <Links />
                         <IndexBlogPreview post={this.props.data.allMarkdownRemark.edges[0].node} />
                     </div>
+                    <hr className={s.hr}/>
+                    <section className={s.about}>
+                        <p><strong>I’m Iiro Jäppinen</strong>. I design user interfaces, icons and logos. Lately I’ve been defining user stories and turning them into valuable experiences.</p>
+                        <p className={s.darkGrey}><strong>Right now</strong> I work at <a href="https://m.verkkokauppa.com" target="_blank" rel="noopener noreferrer"> Verkkokauppa.com</a>, Finland’s largest and best-known online retailer. My goal is to create the best omni-channel shopping experience for all kinds of people.</p>
+                        <p className={s.mediumGrey}><strong>Before that</strong> I helped design the <a href="https://www.humblebundle.com" target="_blank" rel="noopener noreferrer">Humble Indie Bundle</a> brand and concept. I moved to San Francisco in the process but have then returned to Helsinki, Finland. You can reach me online for the occasional icon.</p>
+                    </section>
                 </article>
             </main>
         )
