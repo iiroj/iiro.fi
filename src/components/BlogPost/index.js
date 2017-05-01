062f74b8f3334410043ba58c6791abe1ccc26db8
Iiro Jäppinen <iiro@jappinen.fi>
2017-05-01 14:16:26 +0300

    Fix microdata

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -19,33 +19,33 @@ export default class BlogPost extends Component {
         const postRawDate = post.frontmatter.date
         const postDate = new Date(postRawDate).toDateString()
         const microdata = `{
-            "@context": "http://schema.org",
-            "@type": "BlogPosting",
-            "author": {
-                "@type": "Person",
-                "name": "Iiro Jäppinen"
-            },
-            "publisher": {
-                "@type": "Organization",
-                "name": "iiro.fi",
-                "logo": {
-                    "@type": "ImageObject",
-                    "url": "${profile}",
-                    "height": 384,
-                    "width": 384
-                }
-            },
-            "datePublished": "${postRawDate}",
-            "dateModified": postRawDate,
-            "headline": "${postTitle}",
-            "image": {
-                "@type": "ImageObject",
-                "url": "${profile}",
-                "height": 384,
-                "width": 384
-            },
-            "mainEntityOfPage": "https://iiro.fi${post.slug}"
-        }`
+    "@context": "http://schema.org",
+    "@type": "BlogPosting",
+    "author": {
+        "@type": "Person",
+        "name": "Iiro Jäppinen"
+    },
+    "publisher": {
+        "@type": "Organization",
+        "name": "iiro.fi",
+        "logo": {
+            "@type": "ImageObject",
+            "url": "${profile}",
+            "height": 384,
+            "width": 384
+        }
+    },
+    "datePublished": "${postRawDate}",
+    "dateModified": ${postRawDate},
+    "headline": "${postTitle}",
+    "image": {
+        "@type": "ImageObject",
+        "url": "${profile}",
+        "height": 384,
+        "width": 384
+    },
+    "mainEntityOfPage": "https://iiro.fi${post.slug}"
+}`
 
         return (
             <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,26 @@ import IndexBlog from 'components/IndexBlog'
 
 export default class Index extends Component {
     render () {
-        const IndexMicroData = `
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Person',
-            'name': 'Iiro Jäppinen',
-            'jobTitle': 'UX & UI Designer',
-            'worksFor': 'Fraktio',
-            'url': 'https://iiro.fi/',
-            'email': 'iiro@jappinen.fi',
-            'nationality': 'Finland',
-            'address': {
-                '@type': 'PostalAddress',
-                'addressCountry': 'Finland',
-                'addressLocality': 'Helsinki'
-            },
-            'sameAs': [
-                'https://fi.linkedin.com/in/iiroj',
-                'https://dribbble.com/iiroj',
-                'https://github.com/iiroj'
-            ]
-        }
-        `
+        const IndexMicroData = `{
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "Iiro Jäppinen",
+    "jobTitle": "UX & UI Designer",
+    "worksFor": "Fraktio",
+    "url": "https://iiro.fi/",
+    "email": "iiro@jappinen.fi",
+    "nationality": "Finland",
+    "address": {
+        "@type": "PostalAddress",
+        "addressCountry": "Finland",
+        "addressLocality": "Helsinki"
+    },
+    "sameAs": [
+        "https://fi.linkedin.com/in/iiroj",
+        "https://dribbble.com/iiroj",
+        "https://github.com/iiroj"
+    ]
+}`
 
         return (
             <div>
