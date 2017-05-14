b82902b190b1596fcf9124ed6e614f2d7c718efc
Iiro Jäppinen <iiro@jappinen.fi>
2017-05-14 13:05:36 +0300

    Use external deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+command -v foo >/dev/null 2>&1 || { echo "s3cmd not found. Aborting." >&2; exit 1; }
+
+SOURCE=./public
+TARGET=s3://iiro.fi
+
+s3cmd sync $SOURCE $TARGET  \
+  --delete-removed          \
+  --guess-mime-type         \
+  --no-mime-magic           \
+  --no-preserve             \
+  --server-side-encryption  \
+  --cf-invalidate           \
