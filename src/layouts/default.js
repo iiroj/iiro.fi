7395d9b4dd0eecf244c2a622cfbaf123a8fb9272
Iiro Jäppinen <iiro@jappinen.fi>
2017-05-30 08:13:28 +0300

    Add styled-components to default layout

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react'
-
 import 'reset.css/reset.css'
-import s from './default.module.css'
+import { injectGlobal } from 'styled-components';
 
-export default class DefaultLayout extends Component {
-  render () {
-    const { children } = this.props
-
-    return (
-      <div className={s.root}>
-        {children}
-      </div>
-    )
+injectGlobal`
+  body {
+    background-color: hsla(0, 0%, 95%, 1);
+    color: hsla(0, 0%, 30%, 1);
+    font-family:
+        -apple-system, BlinkMacSystemFont,
+        "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+    font-size: 14px;
+    font-weight: 400;
   }
+
+  a {
+    background: linear-gradient(
+      180deg,
+      hsla(44,100%,75%,0)  0em,
+      hsla(44,100%,75%,0)  0.8em,
+      hsla(44,100%,75%,1) 0.8em,
+      hsla(44,100%,75%,1) 1em
+    );
+    background-repeat: repeat-y;
+    background-size: 100% 1.5rem;
+    color: inherit;
+    text-decoration: none;
+    word-wrap: break-word;
+  }
+
+  a:hover {
+    background: linear-gradient(
+      180deg,
+      hsla(44,100%,75%,0)  0em,
+      hsla(44,100%,75%,0) 0.6em,
+      hsla(44,100%,75%,1) 0.6em,
+      hsla(44,100%,75%,1) 1em
+    );
+    background-repeat: repeat-y;
+    background-size: 100% 1.5rem;
+  }
+
+  a:active {
+    color: hsla(0, 0%, 0%, 1);
+  }
+`
+
+import { Root } from './styled'
+
+export default function DefaultLayout (props) {
+  const { children } = props
+
+  return (
+    <Root>
+      {children}
+    </Root>
+  )
 }
---
+++ src/layouts/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Root = styled.div`
+  line-height: 1.5rem;
+`
