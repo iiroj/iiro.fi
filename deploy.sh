51918651f4e9fc2585db0479d5df470348f31977
Iiro Jäppinen <iiro@jappinen.fi>
2017-07-02 09:40:19 +0300

    Use awscli for syncing to s3

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-command -v s3cmd >/dev/null 2>&1 || { echo "s3cmd not found. Aborting." >&2; exit 1; }
-
 SOURCE=./public/
 TARGET=s3://iiro.fi
 
-s3cmd sync $SOURCE $TARGET  \
-  --delete-removed          \
-  --guess-mime-type         \
-  --no-mime-magic           \
-  --no-preserve             \
-  --server-side-encryption  \
-  --cf-invalidate           \
+aws s3 sync $SOURCE $TARGET --delete --sse
