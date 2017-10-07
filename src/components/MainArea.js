60c85b6776c7bc7e66cc8ffffe9e6cc0fde5e9eb
Iiro Jäppinen <iiro@jappinen.fi>
2017-10-07 11:38:14 +0300

    Main Area mobile styles

--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -28,13 +28,20 @@ export default styled(MainArea)`
     background-color: hsla(0, 0%, 100%, 1);
     content: '';
     display: block;
-    height: 100%;
+    height: 5rem;
     left: 0;
     position: absolute;
     top: 0;
     transform-origin: top left;
-    transform: skewX(-3deg);
-    width: 5rem;
+    transform: skewY(-3deg);
+    width: 100%;
     z-index: -1;
+
+    @media (min-width: 64rem) {
+      height: 100%;
+      transform-origin: top left;
+      transform: skewX(-3deg);
+      width: 5rem;
+    }
   }
 `;
