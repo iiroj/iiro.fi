b1b1c7a592fdb0dcbac41b4e5f1e27eca2b45cee
Iiro Jäppinen <iiro@jappinen.fi>
2018-02-10 15:09:07 +0200

    Automatically get pathMap

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
+const glob = require("glob");
+
+const pageFiles = glob.sync("pages/**/!(_document)@(.js)");
+
+const getPathMap = pageFiles.reduce((pathMap, file) => {
+  const page = file.replace(/^pages/, "");
+  const path = page
+    .replace(/\.js$/, "")
+    .concat("/")
+    .replace(/index\/$/, "");
+
+  pathMap[path] = { page: page };
+  return pathMap;
+}, {});
+
 module.exports = {
-  exportPathMap: function() {
-    return {
-      "/": { page: "/" },
-      "/feedback/": { page: "/feedback" },
-      "/portfolio/": { page: "/portfolio" },
-    };
-  },
+  exportPathMap: () => getPathMap,
   webpack(config, options) {
     const { dev } = options;
 
