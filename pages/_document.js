35659ec50b439cdc48eb2a5dab83a0e2ec48bd7a
Iiro Jäppinen <iiro@jappinen.fi>
2018-02-28 11:25:35 +0200

    Update eslint with babel-eslint

--- pages/_document.js
+++ pages/_document.js
@@ -40,36 +40,28 @@ injectGlobal`
 `;
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps = ({ renderPage }) => {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
-  }
+  };
 
-  render() {
-    const { styleTags } = this.props;
-
-    return (
-      <html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" sizes="600x600" href="/static/icon.png" />
-          <link
-            href="https://fonts.googleapis.com/css?family=Alegreya:400,500"
-            rel="subresource stylesheet"
-            as="style"
-          />
-          {styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+  render = () => (
+    <html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="600x600" href="/static/icon.png" />
+        <link href="https://fonts.googleapis.com/css?family=Alegreya:400,500" rel="subresource stylesheet" as="style" />
+        {this.props.styleTags}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  );
 }
