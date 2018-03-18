0488ec1f338675a9e012356e8996a9a2d78a0990
Iiro Jäppinen <iiro@jappinen.fi>
2018-03-18 20:32:07 +0200

    Seperate util for adding security headers

--- src/server/index.js
+++ src/server/index.js
@@ -2,52 +2,36 @@ require("dotenv").config();
 
 const next = require("next");
 const express = require("express");
-const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const purgeCloudflareCache = require("../utils/purge-cloudflare-cache");
 
 const config = require("../config");
 const { isProduction, port } = config;
 
-const nextJs = next({ dev: !isProduction });
-const handle = nextJs.getRequestHandler();
+const addSecurityHeaders = require("../utils/security-headers");
+const purgeCloudflareCache = require("../utils/purge-cloudflare-cache");
+
+const app = next({ dev: !isProduction });
+const handle = app.getRequestHandler();
 
 const apiRoutes = require("./api");
 
-nextJs
+app
   .prepare()
   .then(() => {
-    const app = express();
+    const server = express();
 
-    app.use(helmet());
+    addSecurityHeaders(server);
 
-    app.use(
-      helmet.referrerPolicy({
-        policy: "no-referrer-when-downgrade",
-      }),
-    );
+    server.use(bodyParser.json());
 
-    app.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          defaultSrc: ["'self'"],
-          fontSrc: ["https://fonts.googleapis.com", "https://fonts.gstatic.com"],
-          scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-          styleSrc: ["https://fonts.googleapis.com", "'unsafe-inline'"],
-        },
-      }),
-    );
+    server.use(morgan("tiny"));
 
-    app.use(bodyParser.json());
+    server.use("/api", apiRoutes);
 
-    app.use(morgan("tiny"));
+    server.get("*", handle);
 
-    app.use("/api", apiRoutes);
-
-    app.get("*", handle);
-
-    app.listen(port, err => {
+    server.listen(port, err => {
       if (err) throw err;
       console.log(`Server ready on http://localhost:${port}`);
     });
