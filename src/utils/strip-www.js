64eee5978097849ee6b225f035cab84a54540e2a
Iiro Jäppinen <iiro@jappinen.fi>
2018-03-30 23:46:09 +0300

    Fix www regex

--- src/utils/strip-www.js
+++ src/utils/strip-www.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
   const host = req.get("host");
 
   if (host.startsWith("www.")) {
-    res.redirect(301, `https://${host.replace(/$www\./, "")}`);
+    res.redirect(301, `https://${host.replace(/^www\./, "")}`);
   } else {
     next();
   }
