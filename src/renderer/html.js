a3235d91927a21a65e0520132f40069103c9f45b
Iiro Jäppinen <iiro@jappinen.fi>
2018-07-02 09:15:30 +0300

    Add manifest.json

--- src/renderer/html.js
+++ src/renderer/html.js
@@ -4,12 +4,15 @@ export default ({ app, helmet, js, state, version }) =>
   '<head>' +
   '<meta charSet="utf-8" />' +
   helmet.title.toString() +
-  '<meta httpEquiv="X-UA-Compatible" content="IE=edge" />' +
-  '<meta name="viewport" content="width=device-width, initial-scale=1.0" />' +
   `<meta name="version" content="${version}" />` +
   helmet.meta.toString() +
+  '<link rel="manifest" href="/manifest.json">' +
   '<link rel="icon" href="/favicon.ico" type="image/x-icon" />' +
-  '<link rel="apple-touch-icon" sizes="600x600" href="/icon.png" />' +
+  '<link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />' +
+  '<link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />' +
+  '<meta httpEquiv="X-UA-Compatible" content="IE=edge" />' +
+  '<meta name="viewport" content="width=device-width, initial-scale=1.0" />' +
+  '<meta name="theme-color" content="#4D4D4D">' +
   helmet.link.toString() +
   '<link rel="preconnect" href="https://fonts.gstatic.com">' +
   js +
