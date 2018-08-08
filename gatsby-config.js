e8bd72e503ec32cf512f5f3142fb89218f8125b3
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-08 16:22:36 +0300

    Use lambda-dev instead of netlify-lambda

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,9 @@
 require('dotenv').config();
 
-const proxy = require('http-proxy-middleware');
-
 module.exports = {
-  developMiddleware: app => {
-    app.use(
-      '/.netlify/functions/',
-      proxy({
-        target: 'http://localhost:9000',
-        pathRewrite: {
-          '/.netlify/functions/': ''
-        }
-      })
-    );
+  proxy: {
+    prefix: '/.netlify/functions',
+    url: 'http://localhost:9000'
   },
 
   plugins: [
