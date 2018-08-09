96061f2c7846b0717d98a292710c3e13fdaf7462
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-09 09:25:38 +0300

    Remove cache headers and trust Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,11 +14,9 @@ module.exports = {
       options: {
         headers: {
           '/*': [
-            'Cache-Control: public, max-age=2592000, no-cache',
             "Content-Security-Policy: default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data:; connect-src 'self' https://fonts.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;",
             'Referrer-Policy: origin-when-cross-origin'
-          ],
-          '/sw.js': ['Cache-Control: max-age=0, no-cache, no-store, must-revalidate', 'Expires: 0', 'Pragma: no-cache']
+          ]
         },
         mergeCachingHeaders: false
       }
