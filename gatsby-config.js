c61731e868262f47f13debd232a6721401800296
Iiro Jäppinen <iiro@jappinen.fi>
2018-09-17 19:54:35 +0300

    fix: Fix Feature-Policy default 'none'

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         headers: {
           '/*': [
             `Content-Security-Policy: ${cspConfig.join(' ')}`,
-            "Feature-Policy: 'none'",
+            "Feature-Policy: default 'none'",
             'Referrer-Policy: no-referrer-when-downgrade'
           ]
         },
