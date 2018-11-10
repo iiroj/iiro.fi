c9af73511b4b3077f2f1af8866d98d820642c7fd
Iiro Jäppinen <iiro@jappinen.fi>
2018-11-10 10:00:54 +0200

    fix: run eslint due to prettier updates

--- src/renderer.js
+++ src/renderer.js
@@ -34,32 +34,41 @@ export default async ({ assets, filename, path, publicPath, stats }) => {
 
   return minify(
     html`
-    <!DOCTYPE html>
-    <html lang="en" ${helmet.htmlAttributes.toString()}>
-      <head>
-        <meta name="version" content="${stats.hash}" />
-        <meta charSet="utf-8" />
-        ${helmet.title.toString()}
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        ${helmet.meta.toString()}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
-        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
-        ${helmet.link.toString()}
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <style>${css}</style>
-        ${scripts.map(
-          src =>
-            `<script type="text/javascript" src="/${src}" rel="subresource" defer></script>`
-        )}
-        <script id="emotion-ids">${JSON.stringify(ids)}</script>
-      </head>
-      <body ${helmet.bodyAttributes.toString()}>
-        <div id="root">${app}</div>
-      </body>
-    </html>
-  `,
+      <!DOCTYPE html>
+      <html lang="en" ${helmet.htmlAttributes.toString()}>
+        <head>
+          <meta name="version" content="${stats.hash}" />
+          <meta charset="utf-8" />
+          ${helmet.title.toString()}
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          ${helmet.meta.toString()}
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
+          <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
+          ${helmet.link.toString()}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <style>
+            ${css}
+          </style>
+          ${
+            scripts.map(
+              src =>
+                `<script type="text/javascript" src="/${src}" rel="subresource" defer></script>`
+            )
+          }
+          <script id="emotion-ids">
+            ${JSON.stringify(ids)};
+          </script>
+        </head>
+        <body ${helmet.bodyAttributes.toString()}>
+          <div id="root">${app}</div>
+        </body>
+      </html>
+    `,
     {
       collapseWhitespace: true,
       preserveLineBreaks: true
