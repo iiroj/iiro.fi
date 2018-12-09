9e229175beb7f1d94ed6f73f1a380be5df586f08
Iiro Jäppinen <iiro@jappinen.fi>
2018-12-09 09:55:04 +0200

    refactor: improve script

--- src/utils/netlifyHeaders.js
+++ src/utils/netlifyHeaders.js
@@ -7,27 +7,26 @@ const staticHeaders = `/*
   Content-Security-Policy: default-src 'none'; connect-src 'self' https://fonts.gstatic.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.cloudfront.net; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   Feature-Policy: default 'none'
   Referrer-Policy: no-referrer-when-downgrade
-  Link: <https://fonts.gstatic.com/s/ibmplexsans/v2/zYXgKVElMYYaJe8bpLHnCwDKhdHeFaxOedc.woff2>; rel=preload; as=font
-  Link: <https://fonts.gstatic.com/s/ibmplexsans/v2/zYX-KVElMYYaJe8bpLHnCwDKhdTuF6ZJW9XjDg.woff2>; rel=preload; as=font
 `;
 
-if (process.env.NODE_ENV === "production") {
-  fs.writeFileSync(headerFilePath, staticHeaders);
-}
-
 const generateLinkHeaders = scripts =>
-  scripts
-    .map(script => `Link: </${script}>; rel=preload; as=script`)
-    .join("\n  ");
+  scripts.map(script => `Link: </${script}>; rel=preload; as=script`);
+
+const generateHeadersForPath = path => headers => `
+${path}
+  ${headers.join("\n  ")}
+`;
+
+var firstRun = true;
 
 export default function generateNetlifyHeaders(path, scripts) {
-  if (process.env.NODE_ENV === "production") {
-    const linkHeaders = generateLinkHeaders(scripts);
-    fs.appendFileSync(
-      headerFilePath,
-      `${path}
-  ${linkHeaders}
-`
-    );
+  if (firstRun) {
+    fs.writeFileSync(headerFilePath, staticHeaders);
+    firstRun = false;
   }
+
+  fs.appendFileSync(
+    headerFilePath,
+    generateHeadersForPath(path)(generateLinkHeaders(scripts))
+  );
 }
