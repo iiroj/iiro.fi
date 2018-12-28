abcab973bef969455eb94c620be3a0775dc32a5a
Iiro Jäppinen <iiro@jappinen.fi>
2018-12-28 12:52:32 +0200

    fix: Aside doesn’t need to be posed

--- src/pages/Portfolio/components/Aside.js
+++ src/pages/Portfolio/components/Aside.js
@@ -1,19 +1,6 @@
-import posed from "react-pose";
 import styled from "styled-components";
 
-const from = {
-  opacity: 0
-};
-
-export default styled(
-  posed.aside({
-    from,
-    enter: {
-      opacity: 1
-    },
-    exit: from
-  })
-)({
+export default styled.aside({
   color: "hsl(0, 0%, 60%)",
   fontSize: "1.5em",
   padding: "4rem 0",
