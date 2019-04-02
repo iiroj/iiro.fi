3e9713bc058edadb12bf56a67ebbc5f7b77eb104
Iiro Jäppinen <iiro@jappinen.fi>
2019-04-02 05:54:58 +0300

    style: use em units

--- src/components/icons/svg.tsx
+++ src/components/icons/svg.tsx
@@ -2,12 +2,12 @@ import { css } from "@emotion/core";
 
 export default css({
   fill: "none",
-  height: "1.5rem",
-  marginRight: "0.5ch",
+  height: "1em",
+  marginRight: "0.25ch",
   stroke: "currentcolor",
   strokeLinecap: "round",
   strokeLinejoin: "round",
   strokeWidth: 2.5,
   verticalAlign: "-12%",
-  width: "1.5rem"
+  width: "1em"
 });
--- src/design/styles.ts
+++ src/design/styles.ts
@@ -6,7 +6,7 @@ export const highlight = css({
   backgroundImage: `linear-gradient(to bottom, ${colors.actionable} 0%, ${
     colors.actionable
   } 100%)`,
-  backgroundPosition: "0 1.25rem",
+  backgroundPosition: "0 0.8em",
   backgroundRepeat: "no-repeat",
   backgroundSize: "100%"
 });
