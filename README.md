fdab06a7819e4736c7f6b6a43f7b26c8610d26ff
Iiro Jäppinen <iiro@jappinen.fi>
2019-04-07 13:25:39 +0300

    ci: deploy to AWS instead of Netlify

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # iiro.fi
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/981f0f46-5476-460f-b143-28246a2c58fa/deploy-status)](https://app.netlify.com/sites/iiro/deploys)
 [![version](https://img.shields.io/github/tag/iiroj/iiro.fi.svg)](https://github.com/iiroj/iiro.fi/releases)
 [![dependencies](https://img.shields.io/david/iiroj/iiro.fi.svg)](https://github.com/iiroj/iiro.fi/blob/master/package.json)
 [![devDependencies](https://img.shields.io/david/dev/iiroj/iiro.fi.svg)](https://github.com/iiroj/iiro.fi/blob/master/package.json)
---
+++ scripts/deploy.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+
+DEPLOY_DIR="public"
+
+if [ ! -d "$DEPLOY_DIR" ]; then
+  echo "Error: Public Directory does not exist"
+  exit 1
+fi
+
+if ${AWS_S3_BUCKET+false}; then
+  echo "Error: \$AWS_S3_BUCKET is not defined"
+  exit 1
+fi
+
+if ${AWS_CLOUDFRONT_ID+false}; then
+  echo "Error: \$AWS_CLOUDFRONT_ID is not defined"
+  exit 1
+fi
+
+# Upload immutable assets
+npx s3-redeploy --cwd $DEPLOY_DIR --bucket $AWS_S3_BUCKET \
+  --pattern '!(render-page).{js,js.map}'                  \
+  --gzip                                                  \
+  --cache 31536000                                        \
+  --immutable
+
+npx s3-redeploy --cwd $DEPLOY_DIR --bucket $AWS_S3_BUCKET \
+  --no-rm                                                 \
+  --pattern 'static/**'                                   \
+  --gzip                                                  \
+  --cache 31536000                                        \
+  --immutable
+
+# Upload cacheable assets
+npx s3-redeploy --cwd $DEPLOY_DIR --bucket $AWS_S3_BUCKET \
+  --no-rm                                                 \
+  --pattern '*.{png,svg,txt,webmanifest}'                 \
+  --cache 630000
+
+# Upload non-cached assets
+npx s3-redeploy --cwd $DEPLOY_DIR --bucket $AWS_S3_BUCKET \
+  --no-rm                                                 \
+  --pattern '!(static)/**/*.{html,json}'                  \
+  --gzip
+
+npx s3-redeploy --cwd $DEPLOY_DIR --bucket $AWS_S3_BUCKET \
+  --no-rm                                                 \
+  --pattern '*.{html,json}'                               \
+  --gzip                                                  \
+  --cf-dist-id $AWS_CLOUDFRONT_ID
+
+# Tag new version
+npx standard-version
+
+# Push to git
+git push --follow-tags origin master
