b92806da6551ce7da5c018b6ab8798e0b1a5a62a
Iiro Jäppinen <iiro@jappinen.fi>
2020-01-05 16:05:29 +0200

    feat: unregister possible leftover Service Workers

---
+++ src/hooks/sw.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const getRegistrations = async () =>
+  (typeof navigator !== "undefined" &&
+    navigator?.serviceWorker?.getRegistrations()) ||
+  [];
+
+export const useServiceWorkerRegistrations = () => {
+  const [registrations, setRegistrations] = useState<
+    readonly ServiceWorkerRegistration[]
+  >([]);
+  getRegistrations().then(registrations => setRegistrations(registrations));
+  return registrations;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { HelmetProvider } from "react-helmet-async";
-import React from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 
 import Reset from "../components/Reset";
+import { useServiceWorkerRegistrations } from "../hooks";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const helmet = React.useRef({});
+
+  const registrations = useServiceWorkerRegistrations();
+  useEffect(() => {
+    if (registrations.length === 0) return;
+    for (const registration of registrations) {
+      console.log("registration", registration);
+      registration.unregister();
+    }
+  }, [registrations]);
 
   return (
     <>
