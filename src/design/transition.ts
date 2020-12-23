7da0398ff1334e73a2eeb6eb25943541f2604428
Iiro Jäppinen <iiro@jappinen.fi>
2020-12-23 15:01:00 +0200

    fix(tsc): update csstype type usage

--- src/design/transition.ts
+++ src/design/transition.ts
@@ -1,10 +1,10 @@
-import { TransitionPropertyProperty } from "csstype";
+import type { Property } from "csstype";
 
 const timing = "ease-in-out";
 const duration = 150;
 
 export const transition = Object.assign(
-  (...props: TransitionPropertyProperty[]) => ({
+  (...props: Property.TransitionProperty[]) => ({
     transition: props
       .map((prop) => `${prop} ${duration}ms ${timing}`)
       .join(", "),
