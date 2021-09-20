58a70aea14118e16b88c1f31b75691ab15a4a245
Iiro Jäppinen <iiro@jappinen.fi>
2021-09-20 08:42:21 +0300

    refactor: improve worker structure

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,4 @@
-import { getAssetFromKV, NotFoundError } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, MethodNotAllowedError, NotFoundError } from '@cloudflare/kv-asset-handler'
 
 const KV_OPTIONS = {
     cacheControl: {
@@ -9,71 +9,71 @@ const KV_OPTIONS = {
 }
 
 /** The static headers to be added to rich HTML responses */
-const STATIC_HEADERS = [
-    [
-        'Content-Security-Policy',
-        [
-            `default-src 'self'`,
-            `connect-src 'self' https://cloudflareinsights.com`,
-            `script-src 'self' https://static.cloudflareinsights.com`,
-            `style-src 'self'`,
-        ].join(';'),
-    ],
-    ['Permissions-Policy', 'interest-cohort=()'] /** Disable Google's FLoC */,
-    ['Referrer-Policy', 'strict-origin-when-cross-origin'],
-    ['Strict-Transport-Security', 'max-age=63072000; includeSubdomains; preload'],
-    ['X-Content-Type-Options', 'nosniff'],
-    ['X-Frame-Options', 'DENY'],
-    ['X-XSS-Protection', '1; mode=block'],
-]
+const staticHeaders = new Headers({
+    'Content-Security-Policy': [
+        `default-src 'self'`,
+        `connect-src 'self' https://cloudflareinsights.com`,
+        `script-src 'self' https://static.cloudflareinsights.com`,
+        `style-src 'self'`,
+    ].join(';'),
+    'Permissions-Policy': 'interest-cohort=()' /** Disable Google's FLoC */,
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+    'Strict-Transport-Security': 'max-age=63072000; includeSubdomains; preload',
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'DENY',
+    'X-XSS-Protection': '1; mode=block',
+})
 
-/** Add static headers to responses with OK status */
-const withResponseHeaders = async (response: Response): Promise<Response> => {
-    if (!response.ok) return response
-
-    for (const [key, value] of STATIC_HEADERS) {
-        response.headers.set(key, value)
+/** Add static headers to a Response */
+const withHeaders = (response: Response): Response => {
+    if (response.ok) {
+        for (const [key, value] of staticHeaders.entries()) {
+            response.headers.set(key, value)
+        }
     }
 
     return response
 }
 
+/** Map a request to the 404 page URL */
+const mapRequestToNotFound = (req: Request) => new Request(`${new URL(req.url).origin}/404.html`, req)
+
 /**
- * Get the rich HTML response to a fetch event:
+ * Get the static response to a fetch event:
  *
- * 1. Get a file from Cloudflare Workers KV storage and assign static headers
- * 1. Else, get the 404 response file and assign its headers
- * 1. Else, respond with error message and code 500
+ * 1. try to get a file from Cloudflare Workers KV storage and assign static headers
+ * 2. if method was not allowed, return error 405
+ * 3. if file was not found, return error 404
+ * 4. return error 500
  */
 const getResponse = async (event: FetchEvent): Promise<Response> => {
     try {
         const response = await getAssetFromKV(event, KV_OPTIONS)
-        return withResponseHeaders(response)
+        return withHeaders(response)
     } catch (error) {
-        if (error instanceof NotFoundError) {
-            const notFoundResponse = await getAssetFromKV(event, {
-                mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/404.html`, req),
+        if (error instanceof MethodNotAllowedError) {
+            return new Response('405 — Method Not Allowed', {
+                headers: { 'Content-Type': 'text/plain' },
+                status: 405,
+                statusText: 'method not allowed',
             })
+        }
 
-            return withResponseHeaders(
-                new Response(notFoundResponse.body, {
-                    ...notFoundResponse,
-                    status: 404,
-                    statusText: 'not found',
-                })
-            )
+        if (error instanceof NotFoundError) {
+            const response = await getAssetFromKV(event, { mapRequestToAsset: mapRequestToNotFound })
+            return withHeaders(new Response(response.body, { ...response, status: 404, statusText: 'not found' }))
         }
 
         return new Response('500 — Internal Error', {
             headers: { 'Content-Type': 'text/plain' },
             status: 500,
-            statusText: 'intrernal error',
+            statusText: 'internal error',
         })
     }
 }
 
 /** Listen to fetch events and get response */
-addEventListener('fetch', async (event: FetchEvent) => {
+addEventListener('fetch', (event: FetchEvent) => {
     const response = getResponse(event)
     return event.respondWith(response)
 })
