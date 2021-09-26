97a3247cde73f39e9571de99790290ee8d3bd3c5
Iiro Jäppinen <iiro@jappinen.fi>
2021-09-26 10:30:57 +0300

    feat: update website icons

--- site/src/components/App.tsx
+++ site/src/components/App.tsx
@@ -23,14 +23,21 @@ const App = ({ helmetContext }: Props) => {
         <HelmetProvider context={helmetContext}>
             <Helmet>
                 <html className={htmlClass} lang="en" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <link rel="icon" href="/favicon.png" type="image/x-icon" />
-                <link rel="apple-touch-icon" href="/icon.png" />
-                <link rel="mask-icon" href="/icon.svg" color="#333333" />
-                <meta name="description" content="Platform Developer at Verkkokauppa.com" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="theme-color" content={raw.background.primary.dark} media="(prefers-color-scheme: dark)" />
-                <meta name="theme-color" content={raw.background.primary.light} media="(prefers-color-scheme: light)" />
+                <meta content="Platform Developer at Verkkokauppa.com" name="description" />
+                <link href="/site.webmanifest" rel="manifest" />
+                <link href="/favicon.ico" rel="alternate icon" type="image/x-icon" />
+                <link color="black" href="/icon.svg" rel="icon mask-icon" type="image/svg+xml" />
+                <link href="/icon-16.png" rel="apple-touch-icon" sizes="16x16" type="image/png" />
+                <link href="/icon-16.webp" rel="apple-touch-icon" sizes="16x16" type="image/webp" />
+                <link href="/icon-32.png" rel="apple-touch-icon" sizes="32x32" type="image/png" />
+                <link href="/icon-32.webp" rel="apple-touch-icon" sizes="32x32" type="image/webp" />
+                <link href="/icon-128.png" rel="apple-touch-icon" sizes="128x128" type="image/png" />
+                <link href="/icon-128.webp" rel="apple-touch-icon" sizes="128x128" type="image/webp" />
+                <link href="/icon-512.png" rel="apple-touch-icon" sizes="512x512" type="image/png" />
+                <link href="/icon-512.webp" rel="apple-touch-icon" sizes="512x512" type="image/webp" />
+                <meta content="width=device-width, initial-scale=1" name="viewport" />
+                <meta content={raw.background.primary.dark} media="(prefers-color-scheme: dark)" name="theme-color" />
+                <meta content={raw.background.primary.light} name="theme-color" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <script type="application/ld+json" data-rh="true">
                     {JSON.stringify(microdata)}
