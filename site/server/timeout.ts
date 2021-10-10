df97859cbcbf8f480c1d240e6b5032f3cb2ccfd2
Iiro Jäppinen <iiro@jappinen.fi>
2021-10-10 17:38:34 +0300

    chore: remove unused files

--- site/server/timeout.ts
+++
@@ -1,7 +0,0 @@
-export const timeout = (ms: number, callback: () => void) =>
-    new Promise<void>((_, reject) => {
-        setTimeout(() => {
-            callback()
-            reject(new Error('Timeout'))
-        }, ms)
-    })
