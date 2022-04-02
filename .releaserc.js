487722dd665d89e2760c6d0a0cb9e1db9682acd1
Iiro Jäppinen <iiro@jappinen.fi>
2022-04-02 15:27:29 +0300

    fix: bump package.json version number on release

--- .releaserc.js
+++ .releaserc.js
@@ -1,19 +1,10 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'conventionalcommits',
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                preset: 'conventionalcommits',
-            },
-        ],
+        ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+        ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
         '@semantic-release/changelog',
+        ['@semantic-release/npm', { npmPublish: false }],
         [
             '@semantic-release/git',
             {
