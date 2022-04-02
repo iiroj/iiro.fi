df5e74909a0c869ee5bbb2604445d7793c2ad384
Iiro Jäppinen <iiro@jappinen.fi>
2022-04-02 10:11:02 +0300

    feat: update React 18, other dependencies

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@
 const headers = require('./next/headers')
 
 const config = {
+    experimental: {
+        runtime: 'edge',
+    },
     exportPathMap: async (defaultPaths) => defaultPaths,
     headers,
     poweredByHeader: false,
