ddecc5b337408f6aab7830f52adbe067dbc0002a
Iiro Jäppinen <iiro@jappinen.fi>
2022-04-02 14:59:10 +0300

    build: add semantic-release config

---
+++ .releaserc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: [],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+    repositoryUrl: process.env.GITHUB_ACTIONS
+        ? 'https://github.com/iiroj/iiro.fi'
+        : 'git@github.com:iiroj/iiro.fi.git',
+}
