2187c6776ae3c5a7c9fb82bd1da41a2b81ea5483
Iiro Jäppinen <iiro@jappinen.fi>
2022-07-10 11:37:06 +0300

    refactor(next): remove `_document.tsx`

--- next.config.js
+++ next.config.js
@@ -1,19 +1,36 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+const isProduction = process.env.NODE_ENV === 'production'
 
-const headers = require('./next/headers')
-
-const config = {
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
     eslint: {
         ignoreDuringBuilds: true,
     },
     experimental: {
         runtime: 'edge',
     },
-    headers,
+    /* eslint-disable-next-line @typescript-eslint/require-await  */
+    headers: async () => [
+        {
+            source: '/:path*',
+            headers: [
+                isProduction ? { key: 'Content-Security-Policy', value: `default-src 'self'` } : null,
+                { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+                { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload' },
+                { key: 'X-Content-Type-Options', value: 'nosniff' },
+                { key: 'X-Frame-Options', value: 'DENY' },
+                { key: 'X-XSS-Protection', value: '1; mode=block' },
+            ].filter(Boolean),
+        },
+    ],
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+        localeDetection: false,
+    },
     swcMinify: true,
     typescript: {
         ignoreBuildErrors: true,
     },
 }
-
-module.exports = config
