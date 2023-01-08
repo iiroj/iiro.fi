dcf70ec333dfa6ae7db4e8c35744a849c1dc7ec7
Iiro Jäppinen <iiro@jappinen.fi>
2023-01-08 16:35:06 +0200

    fix: use correct format for CSP header

--- server/headers.ts
+++ server/headers.ts
@@ -27,10 +27,8 @@ export const getHeaders = (init: Headers) => {
             'Content-Security-Policy',
             [
                 `default-src 'self'`,
-                `script-src 'self'`,
-                `'unsafe-inline'`,
                 /** @see https://static.cloudflareinsights.com/beacon.min.js */
-                `sha512-d1a843625f3ae943212ae61c5b4efc49c31a957aad1498209bb4e6954b69d14943e1e424d08c5f9e6e729172af1893458cb328a2bb5db1e4dfb114fca027e018`,
+                `script-src 'self' 'unsafe-inline' sha512-d1a843625f3ae943212ae61c5b4efc49c31a957aad1498209bb4e6954b69d14943e1e424d08c5f9e6e729172af1893458cb328a2bb5db1e4dfb114fca027e018`,
             ].join(';')
         )
     }
