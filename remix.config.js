e6ead1d2f40a4676a2344c167078d0d8dad1981a
Iiro Jäppinen <iiro@jappinen.fi>
2023-01-21 16:39:37 +0200

    refactor: rename directory to worker/

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
 export default {
     /** @see LiveReload in `app/root.tsx` */
     devServerPort: 8002,
-    server: './server/index.ts',
+    server: './worker/index.ts',
     serverBuildTarget: 'cloudflare-workers',
 
     future: {
