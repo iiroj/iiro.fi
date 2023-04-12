437f98ce7d8c2f31ade42e805b6b9d05e459e8a6
Iiro Jäppinen <iiro@jappinen.fi>
2023-04-12 18:44:02 +0300

    perf: use `createEventHandler` instead of custom function

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,4 @@
-import { createRequestHandler } from '@remix-run/cloudflare';
-import { handleAsset } from '@remix-run/cloudflare-workers';
+import { createEventHandler } from '@remix-run/cloudflare-workers';
 import * as build from '@remix-run/dev/server-build';
 
-const handleRequest = createRequestHandler(build);
-
-const handleEvent = async (event: FetchEvent) => {
-    const asset = await handleAsset(event, build);
-    if (asset) return asset;
-
-    return handleRequest(event.request);
-};
-
-addEventListener('fetch', (event: FetchEvent) => {
-    try {
-        event.respondWith(handleEvent(event));
-    } catch (error: unknown) {
-        event.respondWith(new Response('Internal Server Error', { status: 500 }));
-    }
-});
+addEventListener('fetch', createEventHandler({ build }));
