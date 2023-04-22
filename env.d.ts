bf3aafc2e652391847ccb051c7b91a5def9d7ec2
Iiro Jäppinen <iiro@jappinen.fi>
2023-04-22 18:46:41 +0300

    feat!: migrate to static Cloudflare Pages

--- env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="@remix-run/dev" />
-/// <reference types="@remix-run/cloudflare/dist/globals" />
-/// <reference types="@cloudflare/workers-types" />
-
-declare const CF_BEACON_TOKEN: string;
