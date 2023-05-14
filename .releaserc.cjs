01e216ce5ad3feb1630a41975a388c9e95542cf5
Iiro Jäppinen <iiro@jappinen.fi>
2023-05-14 19:48:42 +0300

    fix(ci): format CHANGELOG.md before committing

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -6,7 +6,18 @@ module.exports = {
       "@semantic-release/release-notes-generator",
       { preset: "conventionalcommits" },
     ],
-    "@semantic-release/changelog",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "npx prettier --write CHANGELOG.md",
+      },
+    ],
     ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/git",
