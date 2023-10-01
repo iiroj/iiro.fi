a601538de5b9ec0a0e5d55745f12fbd9d793fc6c
Iiro Jäppinen <iiro@jappinen.fi>
2023-10-01 10:15:35 +0300

    ci(release): go back to semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,43 @@
+/** @type {import('semantic-release').Options} */
+export default {
+  branches: ["main"],
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/release-notes-generator",
+      { preset: "conventionalcommits" },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "npx prettier --write CHANGELOG.md",
+      },
+    ],
+    ["@semantic-release/npm", { npmPublish: false }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package-lock.json", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}\n\n[skip ci]",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        failTitle: false,
+        labels: false,
+        releasedLabels: false,
+      },
+    ],
+  ],
+  repositoryUrl: process.env.GITHUB_ACTIONS
+    ? "https://github.com/iiroj/iiro.fi"
+    : "git@github.com:iiroj/iiro.fi.git",
+};
