555711f3e22c057815a2a67ce5a99e6ebf3e2422
Iiro Jäppinen <iiro@jappinen.fi>
2025-03-16 16:21:07 +0200

    build: sync storage zone replication setting

--- terraform/storage_zone.tf
+++ terraform/storage_zone.tf
@@ -1,7 +1,8 @@
 resource "bunnynet_storage_zone" "iiro" {
-  name      = "iiro"
-  zone_tier = "Edge"
-  region    = data.bunnynet_region.default.region_code
+  name                = "iiro"
+  zone_tier           = "Edge"
+  region              = data.bunnynet_region.default.region_code
+  replication_regions = ["NY", "SE"]
 
   custom_404_file_path = "/404.html"
 }
