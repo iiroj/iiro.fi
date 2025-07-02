30b4687de09f52c286895d70bbb3873e158af83d
Iiro Jäppinen <iiro@jappinen.fi>
2025-07-02 11:40:45 +0300

    feat: update favicon setup
    Based on https://dev.to/masakudamatsu/favicon-nightmare-how-to-maintain-sanity-3al7

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -7,12 +7,9 @@ type Integrity = {
 const Head: FC<{ integrity: Integrity }> = ({ integrity }) => (
   <>
     <meta charSet="utf-8" />
-    <link
-      href="/static/styles.css"
-      rel="stylesheet"
-      integrity={integrity.styles}
-    />
-    <link rel="apple-touch-icon" href="/static/icon-512.png" />
+    <link rel="icon" href="/favicon.ico" sizes="48x48" />
+    <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     <meta name="description" content="Principal Engineer at SOK" />
     <meta content="width=device-width,initial-scale=1" name="viewport" />
     <meta property="og:image" content="/static/icon-512.png" />
@@ -25,6 +22,11 @@ const Head: FC<{ integrity: Integrity }> = ({ integrity }) => (
       content="I build web experiences, develop tooling, and maintain open-source libraries."
     />
     <meta property="og:url" content="https://iiro.fi" />
+    <link
+      href="/static/styles.css"
+      rel="stylesheet"
+      integrity={integrity.styles}
+    />
   </>
 );
 
